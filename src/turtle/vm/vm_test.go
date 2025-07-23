f061ee41dccd9855233712473d9af30ff4dcb918
Toan <bachmatoan2910@gmail.com>
2025-07-23 18:51:45 -0400

    Added unit test for virtual machine testing Add Arithmetic operation and integer immutation and lexer, and parser outputs

---
+++ src/turtle/vm/vm_test.go
@@ -0,0 +1,85 @@
+package vm
+
+import (
+	"fmt"
+	"testing"
+	"turtle/ast"
+	"turtle/compiler"
+	"turtle/lexer"
+	"turtle/object"
+	"turtle/parser"
+)
+
+type vmTestCase struct {
+	input    string
+	expected interface{}
+}
+
+func TestIntegerArithmetic(t *testing.T) {
+	tests := []vmTestCase{
+		// {"1", 1},
+		// {"2", 2},
+		{"1 + 2", 3}, // the first element in the stack, later expected to be the result of 1 + 2
+	}
+
+	runVmTests(t, tests)
+}
+func runVmTests(t *testing.T, tests []vmTestCase) {
+	t.Helper()
+
+	for _, tt := range tests {
+		// get the ast program
+		program := parse(tt.input)
+
+		// init the compiler
+		comp := compiler.New()
+
+		// run the compiler on the ast program
+		err := comp.Compile(program)
+		if err != nil {
+			t.Fatalf("compiler err: %s", err)
+		}
+		// init the vm (not implemented yet)
+		vm := New(comp.Bytecode())
+		fmt.Printf("runVmTests: %q", vm.instructions)
+		// run the vm
+		err = vm.Run()
+		if err != nil {
+			t.Fatalf("vm err: %s", err)
+		}
+
+		// get the first element in the stack
+		stackElem := vm.StackTop()
+		testExpectedObject(t, tt.expected, stackElem)
+	}
+}
+
+func testExpectedObject(t *testing.T, expected interface{}, actual object.Object) {
+	t.Helper()
+	switch expected := expected.(type) {
+	case int:
+		err := testIntegerObject(int64(expected), actual)
+		if err != nil {
+			t.Errorf("testIntegerObject failed: %s", err)
+		}
+	}
+}
+
+func parse(input string) *ast.Program {
+	l := lexer.New(input)
+	p := parser.New(l)
+	return p.ParseProgram()
+}
+
+func testIntegerObject(expected int64, actual object.Object) error {
+	result, ok := actual.(*object.Integer)
+	if !ok {
+		return fmt.Errorf("object is not Integer. got=%T (%+v)", actual, actual)
+	}
+
+	if result.Value != expected {
+		return fmt.Errorf("object has wrong value. expected=%d, got=%d", expected, result.Value)
+	}
+
+	return nil
+}
