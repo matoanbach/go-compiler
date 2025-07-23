6a3bdf7a8caa4c187b6be5389958f06d7d994485
Toan <bachmatoan2910@gmail.com>
2025-07-23 18:48:48 -0400

    Modified compiler so that it can compile Expression with OpAdd and OpConstant

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -1,6 +1,7 @@
 package compiler
 
 import (
+	"fmt"
 	"turtle/ast"
 	"turtle/code"
 	"turtle/object"
@@ -19,6 +20,42 @@ func New() *Compiler {
 }
 
 func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
+	case *ast.Program:
+		for _, statement := range node.Statements {
+			err := c.Compile(statement)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.ExpressionStatement:
+		err := c.Compile(node.Expression)
+		if err != nil {
+			return err
+		}
+	case *ast.InfixExpression:
+		err := c.Compile(node.Left)
+		if err != nil {
+			return err
+		}
+
+		err = c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+
+		switch node.Operator {
+		case "+":
+			c.emit(code.OpAdd)
+		default:
+			return fmt.Errorf("unkwown operator %s", node.Operator)
+		}
+
+	case *ast.IntegerLiteral:
+		integer := &object.Integer{Value: node.Value}
+		c.emit(code.OpConstant, c.addConstant(integer))
+	}
 	return nil
 }
 
@@ -27,6 +64,23 @@ func (c *Compiler) Bytecode() *Bytecode {
 		Instructions: c.instructions,
 		Constants:    c.constants,
 	}
+}
+
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
+}
+
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
+	pos := c.addInstructions(ins)
+	return pos
+}
+
+func (c *Compiler) addInstructions(ins []byte) int {
+	posNewInstruction := len(c.instructions)
+	c.instructions = append(c.instructions, ins...)
+	return posNewInstruction
 }
 
 type Bytecode struct {
