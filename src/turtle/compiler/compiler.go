1093f81e5c5783cd4ff594462ad4e49d6e23b41f
Toan <bachmatoan2910@gmail.com>
2025-07-24 19:28:14 -0400

    compile boolean values

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -35,6 +35,13 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
+	case *ast.Boolean:
+		if node.Value {
+			c.emit(code.OpTrue)
+		} else {
+			c.emit(code.OpFalse)
+		}
+
 	case *ast.InfixExpression:
 		err := c.Compile(node.Left)
 		if err != nil {
