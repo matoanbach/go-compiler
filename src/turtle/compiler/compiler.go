6ee008f9a1c7f1e3f7a59b304cd1918bbf1f2f62
Toan <bachmatoan2910@gmail.com>
2025-07-24 20:34:54 -0400

    Compiler handles OpEqual, OpNotEqual and OpGreaterThan

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -35,6 +35,7 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
+
 	case *ast.Boolean:
 		if node.Value {
 			c.emit(code.OpTrue)
@@ -43,6 +44,20 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 	case *ast.InfixExpression:
+		if node.Operator == "<" {
+			err := c.Compile(node.Right)
+			if err != nil {
+				return err
+			}
+
+			err = c.Compile(node.Left)
+			if err != nil {
+				return err
+			}
+			c.emit(code.OpGreaterThan)
+			return nil
+		}
+
 		err := c.Compile(node.Left)
 		if err != nil {
 			return err
@@ -62,6 +77,12 @@ func (c *Compiler) Compile(node ast.Node) error {
 			c.emit(code.OpDiv)
 		case "*":
 			c.emit(code.OpMul)
+		case "==":
+			c.emit(code.OpEqual)
+		case "!=":
+			c.emit(code.OpNotEqual)
+		case ">":
+			c.emit(code.OpGreaterThan)
 		default:
 			return fmt.Errorf("unkwown operator %s", node.Operator)
 		}
