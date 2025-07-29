75935f3684b26fb795c95b15c3ab52f6277d04f2
Toan <bachmatoan2910@gmail.com>
2025-07-28 20:17:44 -0400

    fix symbol_table.go

--- src/turtle/compiler/symbol_table.go
+++ src/turtle/compiler/symbol_table.go
@@ -23,7 +23,7 @@ func NewSymbolTable() *SymbolTable {
 }
 
 func (s *SymbolTable) Define(name string) Symbol {
-	symbol := Symbol{Name: name, Scope: GlobalScope, Index: s.numDefinitions}
+	symbol := Symbol{Name: name, Index: s.numDefinitions, Scope: GlobalScope}
 	s.store[name] = symbol
 	s.numDefinitions++
 	return symbol
