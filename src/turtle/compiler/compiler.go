468c6cca56da96995c2bfeae097551536fb361e8
Toan <bachmatoan2910@gmail.com>
2025-07-29 17:49:52 -0400

    compiler emits instructions for OpHash and Hash literals

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -2,6 +2,7 @@ package compiler
 
 import (
 	"fmt"
+	"sort"
 	"turtle/ast"
 	"turtle/code"
 	"turtle/object"
@@ -206,6 +207,29 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 		c.emit(code.OpArray, len(node.Elements))
 
+	case *ast.HashLiteral:
+		keys := []ast.Expression{}
+		for k := range node.Pairs {
+			keys = append(keys, k)
+		}
+
+		sort.Slice(keys, func(i, j int) bool {
+			return keys[i].String() < keys[j].String()
+		})
+
+		for _, key := range keys {
+			err := c.Compile(key)
+			if err != nil {
+				return err
+			}
+
+			err = c.Compile(node.Pairs[key])
+			if err != nil {
+				return err
+			}
+		}
+
+		c.emit(code.OpHash, len(keys)*2)
 	}
 	return nil
 }
