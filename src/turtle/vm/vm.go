81a6f01b53528900c17d61c07c08ad821d59b4d9
Toan <bachmatoan2910@gmail.com>
2025-08-01 13:59:05 -0400

    updated every places use ip

--- src/turtle/vm/vm.go
+++ src/turtle/vm/vm.go
@@ -73,7 +73,7 @@ func (vm *VM) Run() error {
 		// decode
 		case code.OpConstant:
 			constIndex := code.ReadUint16(ins[ip+1:])
-			ip += 2
+			vm.currentFrame().ip += 2
 
 			// execute
 			err := vm.push(vm.constants[constIndex])
@@ -129,27 +129,27 @@ func (vm *VM) Run() error {
 			pos := int(code.ReadUint16(ins[ip+1:]))
 
 			// go to the consequence by default
-			ip += 2
+			vm.currentFrame().ip += 2
 
 			// go to the alternative or outside of the if-else
 			condition := vm.pop()
 			if !isTruthy(condition) {
-				ip = pos - 1
+				vm.currentFrame().ip = pos - 1
 			}
 
 		case code.OpJump:
 			// jump pass the alternative
 			pos := int(code.ReadUint16(ins[ip+1:]))
-			ip = pos - 1
+			vm.currentFrame().ip = pos - 1
 
 		case code.OpSetGlobal:
 			globalIndex := code.ReadUint16(ins[ip+1:])
-			ip += 2
+			vm.currentFrame().ip += 2
 			vm.globals[globalIndex] = vm.pop()
 
 		case code.OpGetGlobal:
 			globalIndex := code.ReadUint16(ins[ip+1:])
-			ip += 2
+			vm.currentFrame().ip += 2
 			err := vm.push(vm.globals[globalIndex])
 			if err != nil {
 				return err
@@ -157,7 +157,7 @@ func (vm *VM) Run() error {
 
 		case code.OpArray:
 			noElements := int(code.ReadUint16(ins[ip+1:]))
-			ip += 2
+			vm.currentFrame().ip += 2
 
 			array := vm.buildArray(vm.sp-noElements, vm.sp)
 			vm.sp = vm.sp - noElements
@@ -169,7 +169,7 @@ func (vm *VM) Run() error {
 
 		case code.OpHash:
 			noElements := int(code.ReadUint16(ins[ip+1:]))
-			ip += 2
+			vm.currentFrame().ip += 2
 
 			hash, err := vm.buildHash(vm.sp-noElements, vm.sp)
 			vm.sp -= noElements
