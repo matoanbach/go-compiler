7bbf5c26a44bd6c48fbdb21a488a369dc56294cb
Toan <bachmatoan2910@gmail.com>
2025-08-01 16:39:58 -0400

    updated compiler.leaveScope() and compiler.enterScope to pass the test TestCompilerScopes

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -30,6 +30,8 @@ func (c *Compiler) enterScope() {
 	}
 	c.scopes = append(c.scopes, scope)
 	c.scopeIndex++
+
+	c.symbolTable = NewEnclosedSymbolTable(c.symbolTable)
 }
 
 func (c *Compiler) leaveScope() code.Instructions {
@@ -37,6 +39,8 @@ func (c *Compiler) leaveScope() code.Instructions {
 
 	c.scopes = c.scopes[:len(c.scopes)-1]
 	c.scopeIndex--
+
+	c.symbolTable = c.symbolTable.Outer
 	return instructions
 }
 
