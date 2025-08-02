990fbe231417bad1923bd92d71631d8e598e2b51
Toan <bachmatoan2910@gmail.com>
2025-08-02 13:33:18 -0400

    evaluator.go: fixed bugs in evalIdentifier() to be aligned with newError()

--- src/turtle/evaluator/evaluator.go
+++ src/turtle/evaluator/evaluator.go
@@ -292,7 +292,7 @@ func evalIdentifier(
 		return builtin
 	}
 
-	return newError("identifier not found: " + node.Value)
+	return newError("identifier not found: %s", node.Value)
 }
 
 func isTruthy(obj object.Object) bool {
