4fc4fa1c87e78075ae530db4d457be2e1ff75a16
Toan <bachmatoan2910@gmail.com>
2025-08-03 13:28:48 -0400

    loadSymbol() is updated to load the right symbol for FreeScope

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -87,6 +87,8 @@ func (c *Compiler) loadSymbol(s Symbol) {
 		c.emit(code.OpGetLocal, s.Index)
 	case BuiltinScope:
 		c.emit(code.OpGetBuiltin, s.Index)
+	case FreeScope:
+		c.emit(code.OpGetFree, s.Index)
 	}
 }
 
