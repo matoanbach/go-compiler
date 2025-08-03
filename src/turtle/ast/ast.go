51595aa91020b8a25f5e50aa1b24675b5e52b434
Toan <bachmatoan2910@gmail.com>
2025-08-03 14:17:15 -0400

    modified FunctionLiteral that with each function comes with a name

--- src/turtle/ast/ast.go
+++ src/turtle/ast/ast.go
@@ -2,6 +2,7 @@ package ast
 
 import (
 	"bytes"
+	"fmt"
 	"strings"
 	"turtle/token"
 )
@@ -220,6 +221,7 @@ type FunctionLiteral struct {
 	Token      token.Token // The 'fn' token
 	Parameters []*Identifier
 	Body       *BlockStatement
+	Name       string
 }
 
 func (fl *FunctionLiteral) expressionNode()      {}
@@ -232,6 +234,10 @@ func (fl *FunctionLiteral) String() string {
 		params = append(params, p.String())
 	}
 
+	out.WriteString(fl.TokenLiteral())
+	if fl.Name != "" {
+		out.WriteString(fmt.Sprintf("<%s>", fl.Name))
+	}
 	out.WriteString(fl.TokenLiteral())
 	out.WriteString("(")
 	out.WriteString(strings.Join(params, ", "))
