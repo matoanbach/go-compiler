3a8495f28e80ae77dfe22a5d71b8b8fcf72bcb0a
Toan <bachmatoan2910@gmail.com>
2025-08-03 12:16:02 -0400

    every functions are treated as a closure by default, OpConstant -> OpClosure

--- src/turtle/compiler/compiler.go
+++ src/turtle/compiler/compiler.go
@@ -326,7 +326,8 @@ func (c *Compiler) Compile(node ast.Node) error {
 			NumLocals:     numLocals,
 			NumParameters: len(node.Parameters),
 		}
-		c.emit(code.OpConstant, c.addConstant(comfiledFunction))
+		functionIndex := c.addConstant(comfiledFunction)
+		c.emit(code.OpClosure, functionIndex, 0)
 
 	case *ast.ReturnStatement:
 		err := c.Compile(node.ReturnValue)
